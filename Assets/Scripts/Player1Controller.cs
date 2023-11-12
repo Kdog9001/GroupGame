f650001b6a483dc1d233fdebcba44fcf1edefb38
Kdog9001 <146856471+Kdog9001@users.noreply.github.com>
2023-11-12 14:54:35 -0500

    Added Swords
    Added the High, Low, and Block stances

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player1Controller : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 10f;
+    public GameObject HSword;
+    public GameObject LSword;
+    public GameObject BSword;
     void Start()
     {
         
@@ -13,6 +17,52 @@ public class Player1Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        AttackHigh();
+        AttackLow();
+        Block();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+    }
+
+    void Block()
+    {
+        if(Input.GetKey("x")) 
+        {
+            HSword.SetActive(false);
+            LSword.SetActive(false);
+            BSword.SetActive(true);
+        }
+    }
+
+    void AttackHigh()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            HSword.SetActive(true);
+            LSword.SetActive(false);
+            BSword.SetActive(false);
+        }
+    }
+
+    void AttackLow()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HSword.SetActive(false);
+            LSword.SetActive(true);
+            BSword.SetActive(false);
+        }
     }
 }
--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player2Controller : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 10f;
+    public GameObject HSword;
+    public GameObject LSword;
+    public GameObject BSword;
     void Start()
     {
         
@@ -13,6 +17,52 @@ public class Player2Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        AttackHigh();
+        AttackLow();
+        Block();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+    }
+
+    void Block()
+    {
+        if (Input.GetKey("8"))
+        {
+            HSword.SetActive(false);
+            LSword.SetActive(false);
+            BSword.SetActive(true);
+        }
+    }
+
+    void AttackHigh()
+    {
+        if (Input.GetKeyDown("7"))
+        {
+            HSword.SetActive(true);
+            LSword.SetActive(false);
+            BSword.SetActive(false);
+        }
+    }
+
+    void AttackLow()
+    {
+        if (Input.GetKeyDown("4"))
+        {
+            HSword.SetActive(false);
+            LSword.SetActive(true);
+            BSword.SetActive(false);
+        }
     }
 }
